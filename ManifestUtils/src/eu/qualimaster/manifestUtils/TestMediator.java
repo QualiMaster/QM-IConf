b87f65e7aee586614e71bd284d27150ec89128cd
Patrik Pastuschek <pastuschek@uni-hildesheim.de>
2016-06-28 15:21:09 +0200

    Added a mediator.
    Added a mediator for testing purposes, since ivy can't handle
    doubled artifacts with different classifiers.

---
+++ ManifestUtils/src/eu/qualimaster/manifestUtils/TestMediator.java
@@ -0,0 +1,25 @@
+package eu.qualimaster.manifestUtils;
+
+import org.apache.ivy.core.module.descriptor.DependencyDescriptor;
+import org.apache.ivy.core.module.descriptor.DependencyDescriptorMediator;
+
+/**
+ * Mediates potential test dependencies and tags them in order to enable usage of multiple artifacts
+ * with the same id but different classifiers.
+ * @author pastuschek
+ *
+ */
+public class TestMediator implements DependencyDescriptorMediator {
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public DependencyDescriptor mediate(DependencyDescriptor dd) {
+        if (null != dd) {
+            if (dd.getDependencyId().toString().equals("Qualimaster.Events")) {
+                dd.getAttributes().put("m:classifier", "tests");
+            }
+        }
+        return dd;
+    }
+
+}
